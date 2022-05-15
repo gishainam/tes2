9eadbeba9364d2c4fbfeb05b4c32b5c153a6ac24
gishainam <105544766+gishainam@users.noreply.github.com>
2022-05-15 10:56:19 +0700

    Add files via upload

--- mapbox/style.js
+++ mapbox/style.js
@@ -38,7 +38,7 @@ var styleJSON = {
             "type": "line",
             "source": "1_1",
             "layout": {},
-            "paint": {'line-width': 0.9285714285714285, 'line-opacity': 1.0, 'line-color': '#729b6f'}
+            "paint": {'line-width': 0.9285714285714285, 'line-opacity': 1.0, 'line-color': '#beb297'}
         }
 ],
 }
